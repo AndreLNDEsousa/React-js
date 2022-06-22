24255719cfd6ea322d2a7b2387f9cc2cc3a4a3b0
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2022-06-22 12:34:31 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import react from "react"
+import ReactDom from "react-dom"
+import "./style.css"
+
+
+const App = ( ) => {
+
+    return (
+        <div className="App">
+            Hello World
+        </div>
+    )
+}
+
+const rootElement = document.getElementById("root")
+ReactDom.render(<App />, rootElement)
