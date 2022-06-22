c9944aef768c63229655e772a959a0d40b2a522a
ANDRE SOUSA <87583186+AndreLNDEsousa@users.noreply.github.com>
2022-06-22 18:52:09 -0300

    Ex01 concluído

---
+++ EX01/js/main.js
@@ -0,0 +1,12 @@
+
+var nome = " Andre Sousa "
+var idade = " 36 anos "
+var frase = "Japão é o melhor time do mundo"
+// alert (nome + idade +" meu primeiro js")
+
+console.log(nome);
+console.log(idade);
+// Sem fechar a tag também funcionou os testes!!!!
+console.log(frase.replace("Japão", "Brasil"))
+
+alert(frase.replace("Japão", "Brasil"))
